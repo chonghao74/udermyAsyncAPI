0fb443acee972dd7aaca09bf0c3c31f5b4ff34d9
Tim Yu <chonghao.yu@gmail.com>
2023-08-25 15:54:50 +0800

    feature: 1.測試基礎 frontend Fetch Get and backend Get 2.測試基礎 frontend Axios Get and backend Get

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyparser = require("body-parser");
 
 const express = require('express');
 const mongoose = require('mongoose');
+const { default: axios } = require("axios");
 const { Schema } = mongoose;//destroction mongose 中有Schema 參數的物件
 const app = express();//執行express 函示
 
@@ -72,6 +73,67 @@ app.get("/person", async (req, res) => {
 
     res.render("person", { numberData, datas });
 })
+
+
+
+app.get("/xmlhttprequestgetapi/:city", async (req, res) => {
+    let { city } = req.params;
+
+    if (city == null) {
+        city = "London";
+    }
+
+    const dataUrl = `https://api.weatherapi.com/v1/current.json?key=2e7758f384d64736b06154338232208&q=${city}&aqi=no`;
+
+});
+
+app.get("/fetchgetapi/:city", async (req, res) => {
+    let { city } = req.params;
+
+    if (city == null) {
+        city = "London";
+    }
+
+    const dataUrl = `https://api.weatherapi.com/v1/current.json?key=2e7758f384d64736b06154338232208&q=${city}&aqi=no`;
+
+    let resData = await fetch(dataUrl, { method: 'GET' }).then(res => {
+        return res.json();
+    }).catch(e => {
+        return e;
+    });
+
+    // console.log(resData);
+    res.send(resData);
+});
+
+app.get("/axiosgetapi/:city", async (req, res) => {
+    let { city } = req.params;
+    if (city == null) {
+        city = "London";
+    }
+
+    //url
+    const baseURL = "https://api.weatherapi.com/v1/current.json";
+    //作法1
+    // const dataUrl = `${baseURL}?key=2e7758f384d64736b06154338232208&q=${city}&aqi=no`;
+    // console.log(dataUrl);
+    // let resData = await axios.get(dataUrl)
+    //     .then(res => { return res.data })
+    //     .catch(e => { return e })
+    //     .finally(console.log("Axios GO1"));
+
+    //作法2
+    const parameterData = { params: { key: "2e7758f384d64736b06154338232208", q: city, api: "no" } }
+    let resData = await axios.get(baseURL, parameterData)
+        .then(res => { return res.data })
+        .catch(e => { return e })
+        .finally(console.log("Axios GO2"));
+
+
+    res.send(resData);
+
+});
+
 
 app.get("/*", (req, res) => {
 
--- public/js/person.js
+++ public/js/person.js
@@ -14,3 +14,75 @@ function clearAllChildNodes(clearContainor) {
     // }
 
 }
+
+function addLabelevent() {
+    divSelectorlab.addEventListener("click", () => {
+        getAxios('London');
+    });
+}
+
+
+async function getFetch(city) {
+    const dataUrl = `https://api.weatherapi.com/v1/current.json?key=2e7758f384d64736b06154338232208&q=${city}&aqi=yes`;
+
+    console.log(dataUrl);
+
+    await fetch(dataUrl, { method: 'GET' }).then(res => {
+
+        return res.text;
+    })
+        .then(data => {
+            let parser = new DOMParser();
+            let fff = parser.parseFromString(data, "application/xml");
+            console.log(fff);
+        })
+        .catch(e => {
+            console.log("Error " + e);
+            return e;
+        });
+}
+
+async function getFetch(city) {
+    const dataUrl = `https://api.weatherapi.com/v1/current.json?key=2e7758f384d64736b06154338232208&q=${city}&aqi=yes`;
+
+    console.log(dataUrl);
+
+    await fetch(dataUrl, { method: 'GET' }).then(res => {
+
+        return res.json();
+    })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(e => {
+            console.log("Error " + e);
+            return e;
+        });
+}
+
+async function getAxios(city) {
+    const baseURL = "https://api.weatherapi.com/v1/current.json";
+    const parameterData = { params: { key: "2e7758f384d64736b06154338232208", q: city, api: "no" } }
+
+    let resData = await axios.get(baseURL, parameterData)
+        .then(res => { return res })
+        .catch(e => { return e })
+        .finally(console.log("Axios GO2"));
+
+
+    window.console.log("data");
+    window.console.log(resData.data);
+    window.console.log("status");
+    window.console.log(resData.status);
+    window.console.log("headers");
+    window.console.log(resData.headers);
+    window.console.log("statusText");
+    window.console.log(resData.statusText);
+    window.console.log("config");
+    window.console.log(resData.config);
+}
+
+// getFetch('London');
+// getAxios('London');
+addLabelevent();
+
